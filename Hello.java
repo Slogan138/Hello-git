78480ec34fe318f26e64dda5952d41964369cae2
Slogan <slogan138@gmail.com>
2019-05-26 16:53:39 +0900

    bob-feature Branch

--- Hello.java
+++ Hello.java
@@ -1,5 +1,4 @@
-/*
-Copyright [2019] [Hansung University]
+/*Copyright [2019] [Hansung University]
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -11,8 +10,7 @@ Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
-limitations under the License.
-*/
+limitations under the License.*/
 
 public class Hello{
   public static void main(String[] args) {
